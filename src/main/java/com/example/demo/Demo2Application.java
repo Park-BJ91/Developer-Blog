f59c8596a59a6ec9e2fd6d33f55721556e8e6d74
parkbyeoungjun <gothog@naver.com>
2022-11-11 13:45:29 +0900

    tiles upload

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -1,22 +1,26 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@RestController
+@Controller
 public class Demo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
 	
-	@RequestMapping("/")
-	public String helloWorldTest() {
-		return "하랑하울이는 하찮다";
+	@GetMapping("/")
+	public String Test(ModelMap model) throws Exception{
+		// .web이 tiles.xml에서 접미어로 구분하여 적용
+		return "main/index.web";
 	}
 	
 }
