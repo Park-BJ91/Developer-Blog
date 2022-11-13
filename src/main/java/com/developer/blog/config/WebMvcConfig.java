fa3ffb6c2e1618f08fe5854f31b9197aa9e0bec5
hh-joo <hhjoo@mindpro.co.kr>
2022-11-13 23:08:41 +0900

    init

---
+++ src/main/java/com/developer/blog/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.developer.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+
+}
---
+++ src/main/java/com/developer/blog/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.developer.blog.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class WebSecurityConfig {
+
+	@Bean
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.antMatchers("/").permitAll()
+			);
+		return http.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+		return (web) -> web.debug(false)
+			.httpFirewall(allowUrlEncodedSlashHttpFirewall())
+			.ignoring()
+				.antMatchers("/assets/**", "/images/**")
+				.antMatchers(HttpMethod.OPTIONS)
+		;
+	}
+
+	@Bean
+	public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowUrlEncodedSlash(true);
+		firewall.setAllowSemicolon(true);
+		firewall.setAllowUrlEncodedDoubleSlash(true);
+		return firewall;
+	}
+}
